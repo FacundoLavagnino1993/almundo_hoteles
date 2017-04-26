df62a5bffed5a9020bea5912f2207484cb71ed61
FacundoLavagnino <facu9396@gmail.com>
2017-04-26 01:35:23 -0300

    cruster+ header - search - filter -

--- src/server/hotels-server/hotels-service.js
+++ src/server/hotels-server/hotels-service.js
@@ -25,11 +25,42 @@ class hotelsService{
                 "sp": "Spa"
             }
         },{
-            "name":"Aquarius Casino Resort",
-            "stars":"1",
-            "price":450,
-            "image":"http://placehold.it/150/92c952"
-        }];
+             "_id": {
+                 "$oid": "58cb47efe3dcd6c694f67533"
+             },
+             "name": "Petit Palace San Bernardo",
+             "descrip": "Solo la habitación",
+             "stars": "4",
+             "price": 2.145,
+             "images": [
+                 "https://images.almundo.com/201/1000000/10000/6300/6260/6b25e36d_b.jpg",
+                 "https://images.almundo.com/201/1000000/10000/4200/4164/675037ae_b.jpg",
+                 "https://images.almundo.com/201/1000000/10000/8100/8092/8092_106_b.jpg"
+             ],
+             "amenities": {
+                 "wifi": "wifi",
+                 "tel": "telefono",
+                 "sp": "Spa"
+             }
+         },{
+             "_id": {
+                 "$oid": "58cb47efe3dcd6c694f67533"
+             },
+             "name": "Hotel Nuevo Boston",
+             "descrip": "Solo la habitación",
+             "stars": "2",
+             "price": 861,
+             "images": [
+                 "//images.almundo.com/201/1000000/10000/1000/915/915_221_b.jpg",
+                 "//images.almundo.com/201/1000000/10000/1000/915/915_207_b.jpg",
+                 "//images.almundo.com/201/1000000/10000/1000/915/915_250_b.jpg"
+             ],
+             "amenities": {
+                 "wifi": "wifi",
+                 "tel": "telefono",
+                 "sp": "Spa"
+             }
+         }];
 
         return new Promise(function(resolve, reject){
             resolve(_self.hotelsList);
